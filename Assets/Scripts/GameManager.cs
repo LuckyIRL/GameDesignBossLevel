eed28a7ba5892df293096067f5ebd65f1285b2f5
ChocoShoco <145370257+ChocoShoco@users.noreply.github.com>
2024-03-17 16:04:24 +0000

    NewPlayerScene

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,7 +6,7 @@ public class GameManager : MonoBehaviour
 {
     public static GameManager gameManager {  get; private set; }
 
-    public UnitHealth _playerHealth = new UnitHealth(100, 100);
+    //public UnitHealth _playerHealth = new UnitHealth(100, 100);
 
     
     // Start is called before the first frame update
