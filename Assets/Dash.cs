ad5d98aae9323eef4545530f8dca9027b28af820
ChocoShoco <145370257+ChocoShoco@users.noreply.github.com>
2024-03-23 21:24:34 +0000

    CannonAiming

--- Assets/Dash.cs
+++ Assets/Dash.cs
@@ -2,15 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HideMouse : MonoBehaviour
+public class Dash : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        Cursor.visible = false;
-        Cursor.lockState = CursorLockMode.Locked;
-
+        
     }
 
-
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
 }
--- Assets/Scripts/CannonShooting.cs
+++ Assets/Scripts/CannonShooting.cs
@@ -1,27 +1,59 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class CannonShooting : MonoBehaviour
 {
+    public Vector2 turn;
     public GameObject cannonBall;
     public Transform firePoint;
+    private bool cannonActive;
+    public GameObject playerCamera;
+    public GameObject player;
+    public GameObject cannonCamera;
+    
 
     public float force;
     // Start is called before the first frame update
     void Start()
     {
+        cannonActive = false;
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if(cannonActive == true)
         {
-            GameObject bullet = Instantiate(cannonBall, firePoint.position, firePoint.rotation);
-            bullet.GetComponent<Rigidbody>().velocity = firePoint.forward * force * Time.deltaTime;
-        }
-        
+            Cursor.lockState = CursorLockMode.Locked;
+            turn.x += Input.GetAxis("Mouse X");
+            turn.y += Input.GetAxis("Mouse Y");
+            transform.localRotation = Quaternion.Euler(-turn.y, turn.x, 0);
+            if (Input.GetMouseButtonDown(0))
+            {
+                GameObject bullet = Instantiate(cannonBall, firePoint.position, firePoint.rotation);
+                bullet.GetComponent<Rigidbody>().velocity = firePoint.forward * force * Time.deltaTime;
+            }
+            if (Input.GetKeyDown(KeyCode.Q))
+            {
+                ExitCannon();
+            }
+        }       
+    }
+
+    public void ActivateCannon()
+    {
+        cannonActive = true;          
+    }
+
+    public void ExitCannon()
+    {
+        player.SetActive(true);
+        playerCamera.SetActive(true);
+        cannonCamera.SetActive(false);
+        cannonActive = false;
     }
 }
